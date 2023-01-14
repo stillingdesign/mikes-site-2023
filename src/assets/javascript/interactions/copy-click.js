2b7d75d28e6603433070eec1feb482bffe99b198
Mike Stilllling <66682227+stillingdesign@users.noreply.github.com>
2023-01-14 07:27:14 -0600

    clean up

--- src/assets/javascript/interactions/copy-click.js
+++ src/assets/javascript/interactions/copy-click.js
@@ -4,10 +4,11 @@ const hoverableMediaThreshold = window.matchMedia("(min-width: 768px)")
 
 if(document.querySelectorAll(`[data-copy-click]`) && hoverableMediaThreshold.matches) {
     document.querySelectorAll(`[data-copy-click]`).forEach(element => {
-        const customValue = element.getAttribute(`data-copy-value`);
-        const customTitle = element.getAttribute(`data-copy-title`);
-        const customNote  = element.getAttribute(`data-copy-note`);
-        const notification = `<div data-copy-notification class="absolute bottom:n24 block bg:sheet-1 font:2xs uppercase border:1 border:edge leading:100 py:4 px:8 radius:full space:nowrap" style="opacity:0">${customNote}</div>`
+        const customValue  = element.getAttribute(`data-copy-value`);
+        const customTitle  = element.getAttribute(`data-copy-title`);
+        const customNote   = element.getAttribute(`data-copy-note`);
+        const noteOffset   = element.getAttribute(`data-copy-offset`);
+        const notification = `<div data-copy-notification class="absolute block bg:sheet-1 font:2xs uppercase border:1 border:edge leading:100 py:4 px:8 radius:full space:nowrap" style="opacity:0">${customNote}</div>`
 
         element.setAttribute(`title`, customTitle)
         element.addEventListener(`click`, function(event) {
@@ -20,7 +21,8 @@ if(document.querySelectorAll(`[data-copy-click]`) && hoverableMediaThreshold.mat
             function rmNotification() { notificationEl.remove(); }
 
             const tl = gsap.timeline({ onComplete:rmNotification });
-            tl.to(notificationEl, { autoAlpha: 1, duration: 0.5})
+            tl.to(notificationEl, { y: noteOffset, duration: 0.5, ease: `power2.inOut`})
+              .to(notificationEl, { autoAlpha: 1, duration: 1, ease: `power2.inOut`}, `-=0.5`)
               .to(notificationEl, { autoAlpha: 0, duration: 0.5}, `+=1`);
         });
     });
